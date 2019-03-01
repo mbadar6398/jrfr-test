66c6d3b477786419880040f5275f90570403c9e8
hellobadar <badar@vp-digital.com>
2019-03-01 08:21:03 +0700

    success display

---
+++ src/Main.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { Store } from "redux";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import { History } from "history";
+
+import { KataReset } from "@kata-kit/reset";
+
+import { RootStore } from "./interfaces/stores";
+import App from "./modules/core/App";
+
+interface MainProps {
+  store: Store<RootStore>;
+  history: History;
+}
+
+export const Main: React.SFC<MainProps> = ({ store, history }) => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <KataReset>
+        <App />
+      </KataReset>
+    </ConnectedRouter>
+  </Provider>
+);
